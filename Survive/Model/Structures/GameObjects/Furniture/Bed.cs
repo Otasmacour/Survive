c7b1afa8f616c43bf458ed45bfc652e4f4488407
Otasmacour <macourekota7@gmail.com>
2023-08-10 09:25:57 +0200

    Adding furniture as GameObjects classes

---
+++ Survive/Model/Structures/GameObjects/Furniture/Bed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survive
+{
+    class Bed : Furniture
+    {
+        public Bed()
+        {
+            this.symbol = 'b';
+        }
+    }
+}
