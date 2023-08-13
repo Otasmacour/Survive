6d79c4f5f7f48619e30038f4f1909b435482aae3
Otasmacour <macourekota7@gmail.com>
2023-08-13 23:45:09 +0200

    Creating methods pickUp and drop, for using in Item's methods PickUp and Drop.

--- Survive/Model/Structures/GameObjects/Items/Item.cs
+++ Survive/Model/Structures/GameObjects/Items/Item.cs
@@ -12,5 +12,15 @@ namespace Survive
         public abstract void PickUp(Character character);
         public abstract void Drop(Character character);
         public abstract void Use(Character character);
+        void pickUp(Character character)
+        {
+            character.mapWhereIsLocated.twoDArray[character.coordinates.y, character.coordinates.x].Remove(this);
+            character.mapWhereIsLocated.mapInformations.itemsOnMap.Remove(this);
+        }
+        void drop(Character character)
+        {
+            character.mapWhereIsLocated.twoDArray[character.coordinates.y, character.coordinates.x].Add(this);
+            character.mapWhereIsLocated.mapInformations.itemsOnMap.Add(this);
+        }
     }
 }
