8c3498b728759ee86e080b68c1cea7dbbfa00139
Otasmacour <macourekota7@gmail.com>
2023-08-13 20:12:39 +0200

    Changes in the location of files and creating class Inventory

---
+++ Survive/Model/Structures/GameObjects/Characters/Player/Inventory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Survive
+{
+    class Inventory
+    {
+        public List<Item> list;
+        public int inventorySize;
+    }
+}
