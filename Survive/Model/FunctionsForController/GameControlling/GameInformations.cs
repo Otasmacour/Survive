2dfe270806165b63645803ab3d0f7474f7316da0
Otasmacour <macourekota7@gmail.com>
2023-08-15 20:19:41 +0200

    Method for returning adjacent items around the player

--- Survive/Model/FunctionsForController/GameControlling/GameInformations.cs
+++ Survive/Model/FunctionsForController/GameControlling/GameInformations.cs
@@ -19,5 +19,18 @@ namespace Survive
         {
             return mapHelper.returnFunctions.GetDistanceOfTwoMaps(characters.player.mapWhereIsLocated, characters.monster.mapWhereIsLocated);
         }
+        public List<Item> GetItemsAroundPlayer()
+        {
+            List<Item> items = new List<Item>();
+            var adjacentCoordinates = mapHelper.returnFunctions.GetAdjacentCoordinates(characters.player.mapWhereIsLocated.twoDArray, characters.player.coordinates, 4);
+            foreach ( var item in adjacentCoordinates )
+            {
+                if(mapHelper.boolFunctions.ItemThere(characters.player.mapWhereIsLocated.twoDArray, item.Value))
+                {
+                    items.Add(mapHelper.returnFunctions.GetItemThere(characters.player.mapWhereIsLocated, item.Value));
+                }
+            }
+            return items;
+        }
     }
 }
