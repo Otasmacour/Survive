08fdf2f7769ea21471291304f84782c338b0005b
Otasmacour <macourekota7@gmail.com>
2023-08-31 14:30:50 +0200

    Adding digging sounds to the Shovel

--- Survive/Model/Structures/GameObjects/Items/Shovel.cs
+++ Survive/Model/Structures/GameObjects/Items/Shovel.cs
@@ -8,6 +8,7 @@ namespace Survive
 {
     class Shovel : Item
     {
+        Random random = new Random();
         public override string getItemName()
         {
             return "Shovel";
@@ -15,6 +16,7 @@ namespace Survive
         public override bool takesUpSpaceInTheInventory => true;
         public override int noiseLevel => 1;
         public override int floorNumberWhereItemSpawns => 1;
+        List<string> diggingSounds = new List<string> { "DiggingWithShovel1", "DiggingWithShovel2", "DiggingWithShovel3", "DiggingWithShovel4", "DiggingWithShovel5" };
         public override string useSoundFileName => throw new NotImplementedException();
         public override string dropSoundFileName => "FallingTool";
         public Shovel(SoundsController soundsController) : base(soundsController)
@@ -31,6 +33,7 @@ namespace Survive
         }
         public override void Use(Character character, MapHelper mapHelper, Alerts alerts)
         {
+            soundsController.soundsToPLay.Enqueue((character.mapWhereIsLocated, GetSound(diggingSounds[random.Next(diggingSounds.Count)])));
             BuriedChest buriedChest = character.mapWhereIsLocated.twoDArray[character.coordinates.y, character.coordinates.x].OfType<BuriedChest>().FirstOrDefault();
             if(buriedChest != null)
             {
